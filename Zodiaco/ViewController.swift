505eb0de1fdd28cee7e2525050067972ba468640
wiski <wiski2311@gmail.com>
2026-01-14 19:58:08 +0100

    Initial Commit

---
+++ Zodiaco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zodiaco
+//
+//  Created by Tardes on 14/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
